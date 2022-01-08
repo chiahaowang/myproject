730241454b531cf8a09a50aee2eef4ca892560f0
cliff <cliff@acubedt.com.tw>
2022-01-08 21:22:07 +0800

    Initial commit

---
+++ src/main/java/com/hao/Hello.java
@@ -0,0 +1,9 @@
+package com.hao;
+
+public class Hello {
+    public static void main(String[] args) {
+//       System.out.print("Hello world");
+       Person p = new Person();
+       p.hello();
+    }
+}
