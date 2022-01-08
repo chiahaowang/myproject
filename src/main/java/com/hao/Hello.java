c5cafd69dbb16224f46f33560df34ae81422307c
cliff <cliff@acubedt.com.tw>
2022-01-08 23:28:35 +0800

    java and kotlin variable,data type

--- src/main/java/com/hao/Hello.java
+++ src/main/java/com/hao/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //       System.out.print("Hello world");
        Person p = new Person();
        p.hello();
+       int age = 19;
+       Integer age2 = 19;
+       char c = 'a';
+       Character c2 = 'a';
+       byte b = 120;
+       float weight = 50.5f;
+       double height = 1.7;
+       boolean adult = true;
+       boolean enroll = false;
+       String name = "hao";
+
     }
 }
